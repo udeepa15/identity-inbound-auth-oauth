96bebddf39e6572d625af53d2bdc0a4db01fab6c
Udeepa_Gallage <udeepagallage@gmail.com>
2025-11-24 13:46:13 +0530

    Add AUDIENCE constant to OAuth2Constants

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/OAuth2Constants.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/OAuth2Constants.java
@@ -44,6 +44,7 @@ public class OAuth2Constants {
         public static final String JWT = "jwt";
     }
 
+    public static final String AUDIENCE = "audience";
     public static final String GROUPS = "groups";
     public static final String ENTITY_ID = "entity_id";
     public static final String IS_CONSENTED = "is_consented";
